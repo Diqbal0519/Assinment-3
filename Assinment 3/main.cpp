9a22d5258f48c886a1a308bc5ee2f78ad9ad8dff
Duha Iqbal <DIqbal0519@gmail.com>
2024-02-23 22:46:45 -0500

    assignment 3 preferred

--- Assinment 3/main.cpp
+++ Assinment 3/main.cpp
@@ -17,17 +17,25 @@ const int TOTAL_QUIZZES = 10;
 
 int num_quizzes_taken();
 int quiz_score();
-void input_file(ifstream& studentNames, ofstream& inFile);
-double calculate_average(int total_score);
-void output_file(double average, int total_grade, ofstream& outFile);
+void input_file(ifstream& studentNames, ofstream& inFile, int& total_score);
+double calculate_average(int& total_score);
+void output_file(double average, int& total_grade, ofstream& outFile);
 
 int main() {
-    srand(time(0));
+    srand((int)time(NULL));
+    int total_grade = 0;
+    double average = 0;
     ifstream studentNames;
     ofstream inputFile, outputFile;
     studentNames.open("Students_Names.txt");
     inputFile.open("Students_Results.txt");
-    input_file(studentNames, inputFile);
+    for (int i = 0; i < NUM_STUDENTS; i++) {
+        input_file(studentNames, inputFile, total_grade);
+        output_file(average, total_grade, inputFile);
+        total_grade = 0;
+        inputFile << "\n";
+        cout << "\n";
+    }
     return 0;
 }
 
@@ -39,31 +47,25 @@ int quiz_score(){
     return rand() % 21;
 }
 
-void input_file(ifstream& studentNames, ofstream& inFile){
-    int n, score, total_grade = 0;
-    double average = 0;
+void input_file(ifstream& studentNames, ofstream& inFile, int& total_score){
+    int n, score;
     string name;
-    
-    for (int i = 0; i < NUM_STUDENTS; i++) {
-        getline(studentNames, name);
-        n = num_quizzes_taken();
-        inFile << name << " ";
-        for(int i = 0; i < n; i++){
-            score = quiz_score();
-            inFile << " " << score;
-            total_grade += score;
-        }
-        output_file(average, total_grade, inFile);
-        total_grade = 0;
-        inFile << "\n";
+    getline(studentNames, name);
+    n = num_quizzes_taken();
+    inFile << name << " ";
+    for(int i = 0; i < n; i++){
+        score = quiz_score();
+        inFile << " " << score;
+        total_score += score;
     }
 }
 
-double calculate_average(int total_score){
+double calculate_average(int& total_score){
     return (double)total_score / (double)TOTAL_QUIZZES;
 }
 
-void output_file(double average, int total_score, ofstream& inFile){
+void output_file(double average, int& total_score, ofstream& inFile){
     average = calculate_average(total_score);
     inFile << ": " << average;
 }
+
